ef56204e18be5e77d0fda19e407296e52ddeeb8d
STEPHANIE G JOHNSON <STEPHANIEJOHNSON.ESQ@GMAIL.COM>
2023-10-30 23:05:09 -0400

    refactored calculate sales and render sales functions so it would adhere to SRP

--- app.js
+++ app.js
@@ -1,31 +1,34 @@
 'use strict';
 
-
 // Function to generate a random number of customers between min and max
 function randomCustomers(min, max) {
   return Math.floor(Math.random() * (max - min + 1)) + min;
 }
 
-// Function to calculate and render sales data for a location
-function calculateAndRenderSales(location) {
+// Function to calculate sales data for a location
+function calculateSales(location) {
+  const salesData = [];
   for (let i = 0; i < location.hours.length; i++) {
     const customers = randomCustomers(location.minCustomers, location.maxCustomers);
-    const cookies = Math.ceil(customers * location.avgCookiesPerCustomer); // Changed Math.floor to Math.ceil
-    location.cookiesSoldPerHour.push(cookies);
-    location.totalCookies += cookies;
+    const cookies = Math.ceil(customers * location.avgCookiesPerCustomer);
+    salesData.push({ hour: location.hours[i], cookiesSold: cookies });
   }
+  return salesData;
+}
 
+// Function to render sales data for a location
+function renderSales(location, salesData) {
   const ulElement = document.getElementById(`${location.name.toLowerCase()}-sales`);
-  ulElement.innerHTML = ''; //reset
+  ulElement.innerHTML = ''; // Clear the list before rendering
 
-  for (let i = 0; i < location.hours.length; i++) {
+  for (let i = 0; i < salesData.length; i++) {
     const li = document.createElement('li');
-    li.textContent = `${location.hours[i]}: ${location.cookiesSoldPerHour[i]} cookies`;
+    li.textContent = `${salesData[i].hour}: ${salesData[i].cookiesSold} cookies`;
     ulElement.appendChild(li);
   }
 
   const totalLi = document.createElement('li');
-  totalLi.textContent = `Total: ${location.totalCookies} cookies`;
+  totalLi.textContent = `Total: ${salesData.reduce((total, data) => total + data.cookiesSold, 0)} cookies`;
   ulElement.appendChild(totalLi);
 }
 
@@ -36,8 +39,6 @@ const seattle = {
   minCustomers: 23,
   maxCustomers: 65,
   avgCookiesPerCustomer: 6.3,
-  cookiesSoldPerHour: [],
-  totalCookies: 0,
 };
 
 const tokyo = {
@@ -46,8 +47,6 @@ const tokyo = {
   minCustomers: 3,
   maxCustomers: 24,
   avgCookiesPerCustomer: 1.2,
-  cookiesSoldPerHour: [],
-  totalCookies: 0,
 };
 
 const dubai = {
@@ -56,8 +55,6 @@ const dubai = {
   minCustomers: 11,
   maxCustomers: 38,
   avgCookiesPerCustomer: 3.7,
-  cookiesSoldPerHour: [],
-  totalCookies: 0,
 };
 
 const paris = {
@@ -66,8 +63,6 @@ const paris = {
   minCustomers: 20,
   maxCustomers: 38,
   avgCookiesPerCustomer: 2.3,
-  cookiesSoldPerHour: [],
-  totalCookies: 0,
 };
 
 const lima = {
@@ -76,14 +71,17 @@ const lima = {
   minCustomers: 2,
   maxCustomers: 16,
   avgCookiesPerCustomer: 4.6,
-  cookiesSoldPerHour: [],
-  totalCookies: 0,
 };
 
 // Calculate and render sales data for each location
-calculateAndRenderSales(seattle);
-calculateAndRenderSales(tokyo);
-calculateAndRenderSales(dubai);
-calculateAndRenderSales(paris);
-calculateAndRenderSales(lima);
+const salesDataSeattle = calculateSales(seattle);
+const salesDataTokyo = calculateSales(tokyo);
+const salesDataDubai = calculateSales(dubai);
+const salesDataParis = calculateSales(paris);
+const salesDataLima = calculateSales(lima);
 
+renderSales(seattle, salesDataSeattle);
+renderSales(tokyo, salesDataTokyo);
+renderSales(dubai, salesDataDubai);
+renderSales(paris, salesDataParis);
+renderSales(lima, salesDataLima);
